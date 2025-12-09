bbc2266fb31a2a729fd970e87413c27afe311121
Adriel <adrielw0610@gmail.com>
2025-12-09 17:49:17 -0500

    Changed activitied

--- app/src/main/java/com/example/mediadatabase/Media.kt
+++ app/src/main/java/com/example/mediadatabase/Media.kt
@@ -1,17 +1,5 @@
 package com.example.mediadatabase
 
-<<<<<<< HEAD
-class Media {
-
-    private var title : String = ""
-    private var location : String = ""
-    private var rating : Float = 0.0f
-
-    constructor(title : String, location : String, rating : Float) {
-        this.title = title
-        this.location = location
-        this.rating = rating
-=======
 import android.content.Context
 
 class Media {
@@ -27,7 +15,6 @@ class Media {
         var editor = sp.edit()
         editor.putString("USERNAME", username)
         editor.commit()
->>>>>>> 0f5b309 (Updated transition screens and persistent data)
     }
 
 
