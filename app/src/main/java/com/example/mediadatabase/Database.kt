563cb17f97a5d05bd99cc95fe483aa175e47a3d6
November8 <d.mcfawlt@gmail.com>
2025-12-10 02:39:11 -0500

    locally stored list now updates with database

--- app/src/main/java/com/example/mediadatabase/Database.kt
+++ app/src/main/java/com/example/mediadatabase/Database.kt
@@ -1,10 +1,13 @@
 package com.example.mediadatabase
 
 import android.util.Log
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class Database {
+class Database : ChildEventListener {
     var firebase = FirebaseDatabase.getInstance()
     lateinit var reference : DatabaseReference
     lateinit var userReference : DatabaseReference
@@ -26,31 +29,46 @@ class Database {
                 Log.w("MainActivity", "${user.key.toString()}")
                 if (user.key.toString() == username) {
                     createNewUser = false
-                    for (media in user.child("media_list").children) {
-                        val title = media.key
-                        val where = media.child("where").value
-                        val rating = media.child("rating").value
-                        val note = media.child("note").value
-                        val dateWatched = media.child("date_watched").value
-                        if (title != null && dateWatched != null) {
-                            Log.w("MainActivity", title.toString())
-                            mediaList.add(
-                                Media(
-                                    title.toString(),
-                                    where.toString(),
-                                    dateWatched.toString().toLong(),
-                                    rating.toString().toFloat(),
-                                    note.toString()
-                                )
-                            )
-                        }
-                    }
+                    userReference = reference.child(username)
+                    userReference.addChildEventListener(this)
+                    updateMediaList(user.child("media_list"))
                 }
             }
         }
     }
 
+    fun updateMediaList(mlSnapshot : DataSnapshot) {
+        Log.w("MainActivity", "List was updated")
+        mediaList.clear()
+        for (media in mlSnapshot.children) {
+            val title = media.key
+            val where = media.child("where").value
+            val rating = media.child("rating").value
+            val note = media.child("note").value
+            val dateWatched = media.child("date_watched").value
+            if (title != null && dateWatched != null) {
+                Log.w("MainActivity", title.toString())
+                mediaList.add(
+                    Media(
+                        title.toString(),
+                        where.toString(),
+                        dateWatched.toString().toLong(),
+                        rating.toString().toFloat(),
+                        note.toString()
+                    )
+                )
+            }
+        }
+
+        mediaList.sortByDescending { it.dateWatched }
+    }
+
     fun addMedia(media : Media) {
+        if (createNewUser) {
+            userReference = reference.child(username)
+            userReference.addChildEventListener(this)
+        }
+
         val mediaInfo = mapOf<String, Any?>(
             "date_watched" to media.dateWatched,
             "where" to media.where,
@@ -62,5 +80,35 @@ class Database {
             .child(media.title)
             .setValue(mediaInfo)
 
+    }
+
+    override fun onChildAdded(
+        snapshot: DataSnapshot,
+        previousChildName: String?
+    ) {
+        Log.w("MainActivity", "Child updated")
+        updateMediaList(snapshot)
+    }
+
+    override fun onChildChanged(
+        snapshot: DataSnapshot,
+        previousChildName: String?
+    ) {
+        Log.w("MainActivity", "Child changed")
+        updateMediaList(snapshot)
+    }
+
+    override fun onChildRemoved(snapshot: DataSnapshot) {
+        Log.w("MainActivity", "Child removed")
+        updateMediaList(snapshot)
+    }
+
+    override fun onChildMoved(
+        snapshot: DataSnapshot,
+        previousChildName: String?
+    ) {
+    }
+
+    override fun onCancelled(error: DatabaseError) {
     }
 }
